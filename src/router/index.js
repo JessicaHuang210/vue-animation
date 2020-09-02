91b3cfa6c9b1db40569aceb34670a8e020f5fad3
Jessica <jessica.huang@silkrode.com.tw>
2020-09-02 09:29:39 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: "*",
+    name: "404",
+    component: Home
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
